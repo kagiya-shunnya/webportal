d69926c5569910ce6c3bc2818b12b2456c0d09cf
kgy07 <kgy07@DESKTOP-KP1LRJM>
2021-06-29 12:18:48 +0900

    住所検索機能の作成

--- src/main/java/jp/ac/hcs/s3a305/zipcode/ZipCodeController.java
+++ src/main/java/jp/ac/hcs/s3a305/zipcode/ZipCodeController.java
@@ -2,6 +2,7 @@ package jp.ac.hcs.s3a305.zipcode;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ZipCodeController {
+	
+
+    @Autowired
+    private ZipCodeService zipCodeService;
 	/*
 	 * 郵便番号から住所を検索し、結果画面を表示する
 	 * @param zipcode 検索する郵便番号（ハイフンなし）
@@ -19,6 +24,9 @@ public class ZipCodeController {
 	@PostMapping("/zip")
 	public String getZipCode(@RequestParam("zipcode") String zipcode,
 			Principal principal, Model model) {
+		
+		ZipCodeEntity zipCodeEntity = zipCodeService.getZip(zipcode);
+		model.addAttribute("zipCodeEntity",zipCodeEntity);
 		
 		return "zipcode/zipcode";
 	}
---
+++ src/main/java/jp/ac/hcs/s3a305/zipcode/ZipCodeEntity.java
@@ -0,0 +1,25 @@
+package jp.ac.hcs.s3a305.zipcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+/*郵便番号情報検索結果の店舗情報
+ * 各項目のデータ仕様については、APIの仕様を参照する。
+ * １つの郵便番号に複数の住所が紐づく事があるため、リスト構造となっている。
+ * -http://zipcloud.ibsnet.co.jp/doc/api
+ */
+@Data
+public class ZipCodeEntity {
+	
+	/*ステータス*/
+	private String status;
+	
+	/*メッセージ*/
+	private String message;
+	
+	/*郵便番号の情報リスト*/
+	private List<ZipCodeData> results = new ArrayList<ZipCodeData>();
+
+}
